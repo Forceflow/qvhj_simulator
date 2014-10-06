6f0b3c1240045cb90170b74c77a1f0741eaa9a36
Jeroen Baert <jerbae@gmail.com>
2014-10-06 04:50:45 +0200

    Round two added and playable, one quiz played in Main

--- src/qvhj_simulator/Main.java
+++ src/qvhj_simulator/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Player a = new Player("Jeroen");
-		a.smartness = 85;
-		Player b = new Player("Elke");
-		b.smartness = 32;
-		Team t = new Team(a,b);
-		t.playQuestion(2);
-		t.playQuestion(5);
+		Player b = new Player("Tom");
+		Player c = new Player("Elke");
+		Player d = new Player("Katrijn");
+		
+		Quiz q = new Quiz(a, b, c, d);
+		q.play();
 	}
 }
--- src/qvhj_simulator/Quiz.java
+++ src/qvhj_simulator/Quiz.java
@@ -10,8 +10,8 @@ public class Quiz {
 	public Player p2;
 	public Player p3;
 	
+	private Team t0;
 	private Team t1;
-	private Team t2;
 	
 	private Random random_generator = new Random();
 	
@@ -22,8 +22,15 @@ public class Quiz {
 		this.p3 = p3;
 	}
 	
+	/**
+	 * Play a full quiz
+	 */
 	public void play(){
 		playRoundOne();
+		//printScores();
+		generateTeams();
+		playRoundTwo();
+		//printScores();
 		generateTeams();
 	}
 	
@@ -50,6 +57,33 @@ public class Quiz {
 		this.p2.playQuestion(1);
 	}
 	
+	/**
+	 * ROUND 2
+	 * 4 sets of 2 questions are asked, 1 or 2 points per question respectively. When question fails, it goes to other team.
+	 */
+	private void playRoundTwo(){
+		playRoundTwoQuestionSet(t1, t0);
+		playRoundTwoQuestionSet(t0, t1);
+		playRoundTwoQuestionSet(t1, t0);
+		playRoundTwoQuestionSet(t0, t1);
+	}
+	
+	/**
+	 * Play a question set of round 2 (one question for 1 point, one question for 2 points) - if team A answers wrong, question goes to other team
+	 */
+	private void playRoundTwoQuestionSet(Team A, Team B){
+		// Team A plays a question set
+		if(! A.playQuestion(1)){ // if they fail
+			B.playQuestion(1); // Team B gets a try
+		}
+		if(! A.playQuestion(2)){
+			B.playQuestion(2);
+		}
+	}
+	
+	/**
+	 * Generate teams - t1 will always be the team with the player with the lowest score
+	 */
 	private void generateTeams(){
 		// get list of players sorted by score
 		ArrayList<Player> sorted_players = this.getScores();
@@ -61,15 +95,15 @@ public class Quiz {
 		if(partner == 0){ 
 			// last player teams up with player in first place
 			this.t1 = new Team(lastplayer, sorted_players.get(0));
-			this.t2 = new Team(sorted_players.get(1), sorted_players.get(2));
+			this.t0 = new Team(sorted_players.get(1), sorted_players.get(2));
 		} else if (partner == 1) {
 			// last player teams up with player in second place
 			this.t1 = new Team(lastplayer, sorted_players.get(1));
-			this.t2 = new Team(sorted_players.get(0), sorted_players.get(3));
+			this.t0 = new Team(sorted_players.get(0), sorted_players.get(3));
 		} else if (partner == 2) {
 			// last player teams up with player in third place
 			this.t1 = new Team(lastplayer, sorted_players.get(2));
-			this.t2 = new Team(sorted_players.get(0), sorted_players.get(1));
+			this.t0 = new Team(sorted_players.get(0), sorted_players.get(1));
 		}
 	}
 	
@@ -81,6 +115,10 @@ public class Quiz {
 		scores.add(p3);
 		Collections.sort(scores, Collections.reverseOrder());
 		return scores;
+	}
+	
+	public void printScores(){
+		System.out.println(p0.getPoints() + " " + p1.getPoints() + " " + p2.getPoints() + " " + p3.getPoints());
 	}
 
 }
